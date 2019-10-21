d5480db28e9b8754d5dca02e98ddd2df2401bb70
ixnp <enprocopios@gmail.com>
2019-10-21 10:28:00 -0700

    second test

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
-import { exportAllDeclaration } from '@babel/types';
+import  CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
+
+//Enzyme API
+//Static Renders the given component and retruns plain HTML
+//Shallow Render *just* the given component and none of its children
+//Full Dom Render the component and all of its children + lets us modify it afterwards
 
 it('shows a comment box', ()=> {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    const wrapped = shallow(<App />);
 
-    expect(div.innerHTML).toContain('Comment box')
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 
-    ReactDOM.unmountComponentAtNode(div);
+
+});
+
+
+it('shows a comment List', ()=> {
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentList).length).toEqual(1);
+
+
 });
