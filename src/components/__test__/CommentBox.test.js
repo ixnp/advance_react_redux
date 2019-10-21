9f81b2fc1b1ee4746c5944ae3eea699d088233e5
ixnp <enprocopios@gmail.com>
2019-10-21 12:39:20 -0700

    added simulate tests to commentbox

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -13,10 +13,31 @@ beforeEach(()=>{
 
 afterEach(()=>{
     wrapped.mount();
-});
+})
 
 it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
+
+})
+describe('the text area', () => {
+    //describes can help limit the scope of tests
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment'}
+        })
+        wrapped.update();
+    });
+
+    it('has a text area that users can tyle in', () => {
+        //simulate events this is not calling onchange
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    })
+
+    it('has a text should be empty after submit', () => {
+        wrapped.find('form').simulate('submit')
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    })
 
 })
