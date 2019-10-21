8fadec1ffaf99f7c22ef6de5b52905fb4c2de072
ixnp <enprocopios@gmail.com>
2019-10-21 09:30:03 -0700

    installed enzyme

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
+import { exportAllDeclaration } from '@babel/types';
 
 it('shows a comment box', ()=> {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
 
-    console.log(div.innerHTML);
+    expect(div.innerHTML).toContain('Comment box')
 
     ReactDOM.unmountComponentAtNode(div);
 });
