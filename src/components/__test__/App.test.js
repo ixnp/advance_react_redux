73b1d115569282069fc356fb3017e0268183074c
ixnp <enprocopios@gmail.com>
2019-10-21 10:39:00 -0700

    added before each

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from '../App'
-import  CommentBox from '../CommentBox'
-import CommentList from '../CommentList'
+import App from 'components/App'
+import  CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
 //Enzyme API
 //Static Renders the given component and retruns plain HTML
 //Shallow Render *just* the given component and none of its children
 //Full Dom Render the component and all of its children + lets us modify it afterwards
+let wrapped;
+
+beforeEach(()=>{
+    wrapped = shallow(<App />);
+})
 
 it('shows a comment box', ()=> {
-    const wrapped = shallow(<App />);
-
     expect(wrapped.find(CommentBox).length).toEqual(1);
 
 
@@ -19,8 +22,6 @@ it('shows a comment box', ()=> {
 
 
 it('shows a comment List', ()=> {
-    const wrapped = shallow(<App />);
-
     expect(wrapped.find(CommentList).length).toEqual(1);
 
 
